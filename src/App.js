f7753f9448cb021a5b3e480b01877095ff9aebfe
parth thakor <parththakor2610@gmail.com>
2023-10-18 14:22:50 +0530

    Header Added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./Components/Layout/Header";
+
+function App() {
+  return (
+    <>
+      <h2>Let's get started!</h2>
+      <Header />
+    </>
+  );
+}
+
+export default App;
