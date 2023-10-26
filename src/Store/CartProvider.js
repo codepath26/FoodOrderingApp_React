873821b2e18a5578165d320778dcb56f2ee630e0
parth thakor <parththakor2610@gmail.com>
2023-10-26 17:33:00 +0530

    add and remove button work in cart with bump  animation

--- src/Store/CartProvider.js
+++ src/Store/CartProvider.js
@@ -30,7 +30,22 @@ const cartReducer = (state, action)=>{
     }
   }
   if(action.type === "REMOVE"){
+    const existingCartItemIndex = state.items.findIndex(item => item.id === action.id);
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedtotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems ;
+    if(existingItem.amount === 1){
+       updatedItems = state.items.filter(item => item.id !== action.id);
+    }else{
 
+     const updatedItem = {...existingItem,amount : existingItem.amount -1}
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return {
+      items : updatedItems,
+      totalAmount : updatedtotalAmount,
+    }
   }
   return defaultCartState;
 }
